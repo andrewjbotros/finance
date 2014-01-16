7a3cb3864d65c2e4fcbb8f51ff64d0656a317d38
andrewjbotros <andrewjbotros@gmail.com>
2014-01-16 13:37:05 -0500

    Coming along

--- finance.rb
+++ finance.rb
@@ -2,13 +2,29 @@
 require 'Time'
 
 #We want to load up some global various to be used throughout various methods
-@@currentYear = Time::new.year
 
-#The Tax Free Savings Account (TFSA) started in 2009 with contributions for anyone 18 and older capped at 5,000. In 2012, that was increased to 5,500
+@@width = 50
+@@indent = 3
+@@header = (@@width*1/4).to_i
+
+@@currentYear = Time::new.year.to_s
+
+@@eiRate = {}
+@@eiRate["2014"] = 0.0188
+@@eiRate["2013"] = 0.0188
 
 @@eiMax={}
 @@eiMax["2014"] = 48600
 @@eiMax["2013"] = 47400
+
+@@cppRate = {}
+@@cppRate["2014"] = 0.0495
+@@cppRate["2013"] = 0.0495
+
+@@cppMax={}
+@@cppMax["2014"] = 52500
+@@cppMax["2013"] = 51100
+@@cppMax["2012"] = 50100
 
 @@tfsaAmount = {}
 @@tfsaAmount["2014"] = 5500
@@ -22,7 +38,7 @@ require 'Time'
 
 class Finance
 
-	attr_reader :firstName,:lastName,:age,:province,:income,:tfsa,:ei
+	attr_reader :firstName,:lastName,:age,:province,:income,:tfsa,:ei,:cpp
 
 	def initialize (firstName, lastName, age, province, income)
 		@firstName = firstName
@@ -30,18 +46,30 @@ class Finance
 		@age = age
 		@province = province
 		@income = income
-	end
-
-	def tfsa
 		@tfsa = TFSA.new(@age)
-	end
-
-	def ei
 		@ei = EI.new(@income)
+		@cpp = CPP.new(@income)
 	end
 
-	def greeting
-		puts "Hello #{@firstName} #{@lastName}, age #{@age} from #{@province}. You make #{@income}. Good job!"
+	def personalInfo
+		print "\n" + " "*@@header + "PERSONAL INFORMATION\n"
+		print "-"*@@width + "\n"
+		print " "*@@indent + "Name: #{@firstName} #{@lastName}\n"
+		print " "*@@indent + "Age: #{@age}\n"
+		print " "*@@indent + "Province: #{@province}\n"
+		print " "*@@indent + "Income: #{@income}\n"
+		print "-"*@@width + "\n"
+	end
+
+	def payrollDeductions
+		print " "*@@header + "PAYROLL DEDUCTIONS\n"
+		print "-"*@@width + "\n"
+		print " "*@@indent + "CPP Premiums: #{@cpp.premium}\n"
+		print " "*@@indent + "EI Premiums: #{@ei.premium}\n"
+		print " "*@@indent + "Tax (Provincial): #{@province}\n"
+		print " "*@@indent + "Tax (Federal): #{@income}\n"
+		print " "*@@indent + "Total($): #{@income}\n"
+		print "-"*@@width + "\n"
 	end
 	
 end
@@ -55,7 +83,7 @@ class TFSA
 	def room
 		contributionRoom = 0
 		count = @age
-		year = @@currentYear
+		year = @@currentYear.to_i
 
 		if @age < 18
 			puts "Sorry, you must be 18 years of age to contribute to your TFSA."
@@ -76,20 +104,38 @@ class EI
 		@income = income
 	end
 
-	def contributions
+	def premium
 		premium = 0
-		if @income < @@eiMax[@@currentYear.to_s]
-			premium = @income*0.0188
+		if @income < @@eiMax[@@currentYear]
+			premium = @income*@@eiRate[@@currentYear]
 		else
-			premium = 47400*0.0188
+			premium = @@eiMax[@@currentYear]*@@eiRate[@@currentYear]
 		end
-		print "You will pay #{premium} in EI premiums."
+		return premium
+	end
+
+end
+
+class CPP
+
+	def initialize (income)
+		@income = income
+	end
+
+	def premium
+		premium = 0
+		if @income < @@cppMax[@@currentYear]
+			premium = @income*@@cppRate[@@currentYear]
+		else
+			premium = @@cppMax[@@currentYear]*@@cppRate[@@currentYear]
+		end
+		return premium
 	end
 
 end
 
 User = Finance.new("Peter", "Pan", "30", "Ontario", 80000)
-puts User.greeting
-puts User.tfsa.room
-puts User.ei.contributions
+puts User.personalInfo
+puts User.payrollDeductions
+
 
